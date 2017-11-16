800594ea7e466e50d3bc121d5e9b873c2796520f
Andrewww <andreitranca@gmail.com>
2017-11-16 11:56:18 +0200

    Witing the code for the Message class
    Writing the code  as the  specification required:
    The Message class has 2 members, a constructer and a toString method.
    The Main class tests if the the toString method works for 3 instances  of the Message class.

--- at.poo.structs/Main.java
+++ at.poo.structs/Main.java
@@ -2,8 +2,15 @@ package at.poo.structs;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)
+  {
+    Message ms1 = new Message("Iustin", "Ce face ba, esti acasa?");
+		Message ms2 = new Message("Alina Irimia", "Sunt o vedeta. Admirati.ma !!");
+		Message ms3 = new Message("Leopol Costel", "Eu sunt Leopel ... Buna ziua !");
+
+		System.out.println(ms1.toString());
+		System.out.println(ms2.toString());
+		System.out.println(ms3.toString());
 	}
 
 }
--- at.poo.structs/Message.java
+++ at.poo.structs/Message.java
@@ -2,6 +2,18 @@ package at.poo.structs;
 
 public class Message
 {
-	
+  public String sender;
+	public String content;
 
+	public Message(String sender, String content)
+	{
+		this.sender = sender;
+		this.content = content;
+	}
+
+	public String toString()
+	{
+		return sender+": "+content;
+
+	}
 }
